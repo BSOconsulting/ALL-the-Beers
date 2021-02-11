507d74f1f431a7cd8d861a56a08733785399e9c4
Jason Affourtit <jason@bsoconsulting.com>
2021-02-10 21:59:07 -0800

    Beta build 2. Now includes missing files

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'ALL the Beers' do
+pod 'IQKeyboardManagerSwift', '6.5.0'
+end
---
+++ Pods/Target Support Files/Pods-ALL the Beers/Pods-ALL the Beers-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ALL_the_Beers : NSObject
+@end
+@implementation PodsDummy_Pods_ALL_the_Beers
+@end
